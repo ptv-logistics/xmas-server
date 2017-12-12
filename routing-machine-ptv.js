8a4dd31961c487c80ac39b6edfaa5c6d908fbe2c
oliverheilig <oliver.heilig@yahoo.de>
2017-12-12 08:53:35 +0100

    scaling santa

--- routing-machine-ptv.js
+++ routing-machine-ptv.js
@@ -1,159 +1,141 @@
 L.Routing.Ptv = L.Class.extend({
-	options: {
-		// xRoute url
-		serviceUrl: 'https://api.cloud.ptvgroup.com/xroute/rs/XRoute/',
-		// token for xServer internet
-		token: '',
-		// indicates the back-end is a real xServer that supports heading informations
-		supportsHeadings: true,
-		// number of alternatives to calculate
-		numberOfAlternatives: 0,
-		// delegate to manipulate the sent request
-		beforeSend: null
-	},
+    options: {
+        serviceUrl: 'https://xroute-eu-n-test.cloud.ptvgroup.com/xroute/rs/XRoute/',
+        token: '',
+        supportsHeadings: true,
+        numberOfAlternatives: 0,
+        beforeSend: null
+    },
 
-	initialize: function (options) {
-		L.Util.setOptions(this, options);
-	},
+    initialize: function (options) {
+        L.Util.setOptions(this, options);
+        this._hints = {
+            locations: {}
+        };
+    },
 
+    currentRouteIdx: 0,
 
-	runRequest: function (url, request, token, handleSuccess, handleError) {
-		$.ajax({
-			url: url,
-			type: 'POST',
-			data: JSON.stringify(request),
+    hasError: false,
 
-			headers: function () {
-				var h = {
-					'Content-Type': 'application/json'
-				};
-				if (token) h['Authorization'] = 'Basic ' + btoa('xtok:' + token);
-				return h;
-			}(),
+    route: function (waypoints, callback, context, options, currentResponses) {
+        var url = this.options.serviceUrl + 'calculateRoute';
 
-			success: function (data, status, xhr) {
-				handleSuccess(data);
-			},
+        // build current responses on initial call
+        var responses;
+        if (!currentResponses) {
+            responses = [];
+        } else {
+            responses = currentResponses;
+        }
+        var cnt = 0;
+        this.currentRouteIdx++;
+        hasError = false;
 
-			error: function (xhr, status, error) {
-				handleError(xhr);
-			}
-		});
-	},
+        for (var i = 0; i <= this.options.numberOfAlternatives; i++) {
+            (function (t, ii) {
+                var ci = t.currentRouteIdx;
+                var that = t;
+                var request = t._buildRouteRequest(waypoints, options, responses, ii);
+                runRequest(url, request, t.options.token,
 
-	route: function (waypoints, callback, context, options, currentResponses) {
-		var url = this.options.serviceUrl + 'calculateRoute';
+                    function (response) {
+                        if (hasError || t.currentRouteIdx !== ci)
+                            return;
 
-		// build current responses on initial call
-		var responses;
-		if (!currentResponses) {
-			responses = [];
-		} else {
-			responses = currentResponses;
-		}
+                        cnt++;
+                        responses[ii] = response;
 
-		var request = this._buildRouteRequest(waypoints, options, responses)
+                        if (cnt > that.options.numberOfAlternatives) {
+                            that._routeDone(responses, waypoints, callback, context);
+                        }
+                    },
 
-		var geometryOnly = options && options.geometryOnly;
+                    function (xhr) {
+                        if (hasError || t.currentRouteIdx !== ci)
+                            return;
 
-		var numAlts = geometryOnly ? 0 : this.options.numberOfAlternatives;
+                        hasError = true;
 
-		this.runRequest(url, request, this.options.token,
-			L.bind(function (response) {
-				responses.push(response);
+                        console.log(xhr);
+                        callback.call(context, xhr, null);
+                    });
+            })(this, i);
+        }
+    },
 
-				if (responses.length > numAlts) {
-					this._routeDone(responses, waypoints, callback, context);
-				} else {
-					this.route(waypoints, callback, context, options, responses);
-				}
-			}, this),
+    _routeDone: function (responses, inputWaypoints, callback, context) {
+        alts = [];
+        for (var i = 0; i < responses.length; i++) {
+            var response = responses[i];
 
-			function (xhr) {
-				callback.call(context || callback, {
-					status: xhr.status,
-					message: xhr.responseJSON ? xhr.responseJSON.errorMessage : xhr.responseText
-				});
-			}
-		);
-	},
+            var coordinates = this._buildLinestring(response.polygon.lineString.points);
+            alts.push({
+                segments: response.segments,
+                name: 'Route ' + (i + 1),
+                coordinates: coordinates,
+                instructions: this._bulidInstructions(response.manoeuvres, response.segments, response.stations),
+                summary: this._convertSummary(response.info),
+                inputWaypoints: inputWaypoints,
+                waypoints: inputWaypoints,
+                waypointIndices: this._buildWaypointIndices(response.stations)
+            });
+        }
 
-	_routeDone: function (responses, inputWaypoints, callback, context) {
-		var alts = [];
-		for (var i = 0; i < responses.length; i++) {
-			var response = responses[i];
+        if (typeof this.options.routesCalculated === 'function') {
+            this.options.routesCalculated(alts, responses);
+        }
 
-			var coordinates = this._buildLinestring(response.polygon.lineString.points);
-			alts.push({
-				name: 'Route ' + (i + 1),
-				coordinates: coordinates,
-				instructions: this._bulidInstructions(response.manoeuvres, response.segments, response.stations),
-				summary: this._convertSummary(response.info),
-				inputWaypoints: inputWaypoints,
-				waypoints: inputWaypoints,
-				waypointIndices: this._buildWaypointIndices(response.stations)
-			});
-		}
+        callback.call(context, null, alts);
+    },
 
-		callback.call(context, null, alts);
+    _buildWaypointIndices: function (stations) {
+        var waypointIndices = [];
+        for (var i = 0; i < stations.length; i++) {
+            waypointIndices.push(stations[i].polyIdx);
+        }
+    },
 
-		if (typeof this.options.routesCalculated === 'function') {
-			this.options.routesCalculated(alts, responses);
-		}
-	},
-
-	_buildWaypointIndices: function (stations) {
-		var waypointIndices = [];
-		for (var i = 0; i < stations.length; i++) {
-			waypointIndices.push(stations[i].polyIdx);
-		}
-	},
-
-	_drivingDirectionType: function (manoeuvre) {
-		if (!this.options.supportsHeadings) {
-			return '';
-		}
-
-		switch (manoeuvre.manoeuvreType) 
-		{
-			case 'UTURN':
-				return 'TurnAround';
-			case 'ENTER_RA':
-			case 'STAY_RA':
-			case 'EXIT_RA':
-			case 'EXIT_RA_ENTER':
-			case 'EXIT_RA_ENTER_FERRY':
-				return 'Roundabout';
-			case 'FURTHER':
-			case 'KEEP':
-			case 'CHANGE':
-			case 'ENTER':
-			case 'EXIT':
-			case 'ENTER_FERRY':
-			case 'EXIT_FERRY':
-				switch (manoeuvre.turnOrient) {
-					case 'LEFT':
-						return 'SlightLeft';
-					case 'RIGHT':
-						return 'SlightRight';
-					default:
-						return 'Straight';
-				}
-				break;
-			case 'TURN':
-				switch (manoeuvre.turnOrient) {
-					case 'LEFT':
-						return (manoeuvre.turnWeight === 'HALF') ? 'SlightLeft' : (manoeuvre.turnWeight === 'STRONG') ? 'SharpLeft' : 'Left';
-					case 'RIGHT':
-						return (manoeuvre.turnWeight === 'HALF') ? 'SlightRight' : (manoeuvre.turnWeight === 'STRONG') ? 'SharpRight' : 'Right';
-					default:
-						return 'Roundabout';
-				}
-				break;
-			default:
-				return 'Straight';
-		}
-	},
+    _drivingDirectionType: function (manoeuvre) {
+        if (!this.options.supportsHeadings)
+            return '';
+        switch (manoeuvre.manoeuvreType) {
+        case 'UTURN':
+            return 'TurnAround';
+        case 'ENTER_RA':
+        case 'STAY_RA':
+        case 'EXIT_RA':
+        case 'EXIT_RA_ENTER':
+        case 'EXIT_RA_ENTER_FERRY':
+            return 'Roundabout';
+        case 'FURTHER':
+        case 'KEEP':
+        case 'CHANGE':
+        case 'ENTER':
+        case 'EXIT':
+        case 'ENTER_FERRY':
+        case 'EXIT_FERRY':
+            switch (manoeuvre.turnOrient) {
+                case 'LEFT':
+                    return 'SlightLeft';
+                case 'RIGHT':
+                    return 'SlightRight';
+                default:
+                    return 'Straight';
+                }
+        case 'TURN':
+            switch (manoeuvre.turnOrient) {
+                case 'LEFT':
+                    return (manoeuvre.turnWeight == 'HALF') ? 'SlightLeft' : (manoeuvre.turnWeight == 'STRONG') ? 'SharpLeft' : 'Left';
+                case 'RIGHT':
+                    return (manoeuvre.turnWeight == 'HALF') ? 'SlightRight' : (manoeuvre.turnWeight == 'STRONG') ? 'SharpRight' : 'Right';
+                default:
+                    return 'Roundabout';
+                }
+        default:
+            return 'Straight';
+        }
+    },
 
 	_bulidInstructions: function (manoeuvres, segments, stations) {
 		var instructions = [];
@@ -203,78 +185,80 @@ L.Routing.Ptv = L.Class.extend({
 		return instructions;
 	},
 
-	_buildLinestring: function (inputpoints) {
-		var points = [];
+    _buildLinestring: function (inputpoints) {
+        var points = [];
 
-		for (var i = 0; i < inputpoints.length; i++) {
-			points.push([inputpoints[i].y, inputpoints[i].x]);
-		}
+        for (var i = 0; i < inputpoints.length; i++) {
+            points.push([inputpoints[i].y, inputpoints[i].x]);
+        }
 
-		return points;
-	},
+        return points;
+    },
 
-	_convertSummary: function (info) {
-		return {
-			totalDistance: info.distance,
-			totalTime: info.time
-		};
-	},
+    _convertSummary: function (info) {
+        return {
+            totalDistance: info.distance,
+            totalTime: info.time
+        };
+    },
 
-	_buildRouteRequest: function (waypoints, options, currentResponses) {
-		var exceptionPaths = [];
-		if (currentResponses) {
-			for (var i = 0; i < currentResponses.length; i++) {
-				exceptionPaths.push({
-					binaryPathDesc: currentResponses[i].binaryPathDesc,
-					relMalus: 1000
-				});
-			}
-		}
+    _buildRouteRequest: function (waypoints, options, currentResponses, idx) {
+        // var exceptionPaths = [];
+        // if (currentResponses) {
+        // for (var i = 0; i < currentResponses.length; i++) {
+        // exceptionPaths.push(
+        // {
+        // "binaryPathDesc": currentResponses[i].binaryPathDesc,
+        // "relMalus": 1000
+        // });
+        // }
+        // }
 
-		var wpCoords = [];
-		for (i = 0; i < waypoints.length; i++) {
-			wpCoords.push({
-				coords: [{
-					point: {
-						x: waypoints[i].latLng.lng,
-						y: waypoints[i].latLng.lat
-					}
-				}],
-				linkType: 'NEXT_SEGMENT'
-			});
-		}
+        var wpCoords = [],
+            computeInstructions,
+            computeAlternative;
 
-		var geometryOnly = options && options.geometryOnly;
+        for (var i = 0; i < waypoints.length; i++) {
+            wpCoords.push({
+                'coords': [{
+                    'point': {
+                        'x': waypoints[i].latLng.lng,
+                        'y': waypoints[i].latLng.lat
+                    }
+                }],
+                'linkType': 'NEXT_SEGMENT'
+            });
+        }
 
-		var numAlts = geometryOnly ? 0 : this.options.numberOfAlternatives;
+        computeAlternative = computeInstructions = !(options && options.geometryOnly);
 
-		var request = {
-			waypoints: wpCoords,
-			options: [],
-			exceptionPaths: exceptionPaths,
-			details: {
-				binaryPathDesc: currentResponses.length < numAlts, // last route doesn't need exception paths
-				detailLevel: 'STANDARD',
-				polygon: true,
-				manoeuvres: !geometryOnly,
-				segments: !geometryOnly
-			},
-			callerContext: {
-				properties: [{
-					key: 'CoordFormat',
-					value: 'OG_GEODECIMAL'
-				}]
-			}
-		};
+        var request = {
+            'waypoints': wpCoords,
+            'options': [],
+            //            "exceptionPaths": exceptionPaths,
+            'details': {
+                //                binaryPathDesc: currentResponses.length < this.options.numberOfAlternatives, // last route doesn't need exception paths
+                detailLevel: 'STANDARD',
+                polygon: true,
+                manoeuvres: true,
+                segments: true
+            },
+            'callerContext': {
+                'properties': [{
+                    'key': 'CoordFormat',
+                    'value': 'OG_GEODECIMAL'
+                }]
+            }
+        };
 
-		if (typeof this.options.beforeSend === 'function') {
-			request = this.options.beforeSend(request);
-		}
+        if (typeof this.options.beforeSend === 'function') {
+            request = this.options.beforeSend(request, currentResponses, idx);
+        }
 
-		return request;
-	}
+        return request;
+    }
 });
 
 L.Routing.ptv = function (options) {
-	return new L.Routing.Ptv(options);
+    return new L.Routing.Ptv(options);
 };
