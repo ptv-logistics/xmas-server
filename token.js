b719b8a5d197bf8f737f2ec9433cf88f14033139
xserverinternet <oliver.heilig@ptvgroup.com>
2021-02-15 00:02:56 +0100

    Token update

--- token.js
+++ token.js
@@ -1,3 +1,3 @@
 // The token below is for internal use only - consider getting your own token!
-// This token is valid until 2021-04-30T20:39:38Z and was auto-generated @ 2021-02-13T22:39:23Z.
+// This token is valid until 2021-04-30T20:39:38Z and was auto-generated @ 2021-02-14T22:39:21Z.
 var token = '601F872E-C704-4765-B896-DD2138DE8FDB';
